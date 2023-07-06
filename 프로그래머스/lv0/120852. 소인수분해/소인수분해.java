eab03a6b9cf8e756d78a3331a606a9ab39f89e71
kimjiyooniiiii <117561820+kimjiyooniiiii@users.noreply.github.com>
2023-07-06 15:50:45 +0900

    [level 0] Title: 소인수분해, Time: 3.91 ms, Memory: 70 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> list = new ArrayList<>();
+        int start = 2;
+        
+        while(start <= n && n != 1){
+            if(n % start == 0){
+                list.add(start);
+                n /= start;
+            }else{
+                start++;
+            }
+        }
+        List<Integer> newList = list.stream().distinct().collect(Collectors.toList());
+        int[] array = newList.stream().mapToInt(Integer::intValue).toArray();
+        
+        System.out.println(list);
+
+        return array;
+    }
+}
