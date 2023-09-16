100a5bb1cc538131deced112dd312fde8720b7e7
김지윤 <117561820+kimjiyooniiiii@users.noreply.github.com>
2023-09-16 17:04:48 +0900

    [Silver II] Title: 스택 수열, Time: 2540 ms, Memory: 44620 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,50 +1,43 @@
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.Stack;
+import java.io.IOException;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
 
-        int n = scan.nextInt();
-        int[] want = new int[n];
-        for(int i=0 ;i<n; i++) {
-            want[i] = scan.nextInt();
-        }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        Stack<Integer> stack = new Stack<>();
+        int n = Integer.parseInt(br.readLine());
+        List<Integer> standard = new ArrayList<>();
+        Stack<Integer> seqStack = new Stack<>();
         List<String> result = new ArrayList<>();
-        int a = 0;
-        for(int i=1; i<=n; i++) {
-            if(i == want[a]){
-                stack.push(i);
-                result.add("+");
-                stack.pop();
-                result.add("-");
-                a++;
-                //System.out.println(stack + ": " + result + " : " + a);
 
-                while(!stack.isEmpty() && stack.peek() == want[a]){
-                    result.add("-");
-                    stack.pop();
-                    a++;
-                    //System.out.println(stack + ": " + result + " : " + a);
-                }
+        for(int i=0; i<n; i++) {
+            standard.add(Integer.parseInt(br.readLine()));
+        }
+
+        int num = 1;
+        while(!standard.isEmpty()) {
+            if(num > n && !seqStack.peek().equals(standard.get(0))) {
+                break;
+            }
+            else if(!seqStack.isEmpty() && seqStack.peek().equals(standard.get(0))){
+                result.add("-");
+                standard.remove(0);
+                seqStack.pop();
+
             }else{
-                stack.push(i);
                 result.add("+");
-               // System.out.println(stack + ": " + result);
+                seqStack.add(num++);
             }
         }
-        if(stack.isEmpty()){
-            for(int i=0; i<result.size(); i++){
-                System.out.println(result.get(i));
-            }
-            //System.out.println(result);
-        }else{
+
+        if(!standard.isEmpty()) {
             System.out.println("NO");
+        }else{
+            for(String s : result) {
+                System.out.println(s);
+            }
         }
 
     }
