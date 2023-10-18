1bc12f5987c814d241e0d1693ee8a99d45fc04f1
김지윤 <117561820+kimjiyooniiiii@users.noreply.github.com>
2023-10-18 15:43:27 +0900

    [Bronze V] Title: 나머지, Time: 148 ms, Memory: 15812 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String numberLine = br.readLine();
+        StringTokenizer st = new StringTokenizer(numberLine, " ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        bw.write(((a+b)%c)+"\n");
+        bw.write((((a%c)+(b%c))%c)+"\n");
+        bw.write(((a*b)%c)+"\n");
+        bw.write((((a%c)*(b%c))%c)+"\n");
+
+        bw.flush();
+        bw.close();
+    }
+}
