e43dd246beb3aabd7a9b2efa3b6fc5c339291f53
김지윤 <117561820+kimjiyooniiiii@users.noreply.github.com>
2023-10-26 21:09:23 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 144 ms, Memory: 15876 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -7,49 +6,48 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-        String fac = facto(n).toString();
-        int result=0;
-        for(int i=fac.length()-1; i>=0; i--) {
-            if(fac.charAt(i) != '0'){
-                break;
-            }else{
-                result++;
+        int count = 0;
+        for(int i=5; i<=n; i+=5) {
+            int num = i;
+            while(num % 5 == 0){
+                count++;
+                num/=5;
             }
         }
-        bw.write(result + "\n");
+        bw.write(count+" ");
         bw.flush();
+        bw.close();
 
-        /*Stack<Integer> stack = new Stack<>();
-        for(int i=0; i<fac.length(); i++) {
-            stack.push(Character.getNumericValue(fac.charAt(i)));
-        }
-        //System.out.println(stack);
+        /*String[] inputArray = br.readLine().split(" ");
 
-        while(!stack.isEmpty()){
-            if(stack.peek()!=0){
-                bw.write(result + "\n");
-                bw.flush();
-                break;
-            }else{
-                result++;
-                stack.pop();
-            }
-        }
-        if(stack.isEmpty()){
-            bw.write(result + "\n");
-            bw.flush();
-        }
-        bw.close();*/
+        BigInteger first = new BigInteger(inputArray[0]);
+        BigInteger second = new BigInteger(inputArray[1]);
+
+
+
+        //int count = facto(first) / (facto(first-second) * facto(second));
+        System.out.println(combination(first, second));*/
     }
 
+/*    public static BigInteger combination(BigInteger n, BigInteger r) {
+        BigInteger one = new BigInteger("1");
 
-    public static BigInteger facto(int n) {
-        if(n<=1){
-            //System.out.println(n);
-            return BigInteger.valueOf(1);
+        if(n.equals(r) || r.equals(new BigInteger("0"))){
+            return one;
         }else{
-            //System.out.println(n);
-            return BigInteger.valueOf(n).multiply(facto(n-1));
+            return combination(n.subtract(one), r.subtract(one)).add(
+                    combination(n.subtract(one), r));
         }
+        //return facto(n).divide(facto(r).multiply(facto(n.subtract(r))));
     }
+
+    public static BigInteger facto(BigInteger n){
+        BigInteger one = new BigInteger("1");
+
+        if(n.compareTo(one) == 0 || n.compareTo(one)==-1) { // 1보다 작거나 같으면
+            return one;
+        }else{
+            return n.multiply(facto(n.subtract(one)));
+        }
+    }*/
 }
