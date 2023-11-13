d8808864ef4deeafcda51ea9db43d69dddcd1af0
김지윤 <117561820+kimjiyooniiiii@users.noreply.github.com>
2023-11-13 20:56:26 +0900

    [Silver II] Title: 제곱수의 합, Time: 2564 ms, Memory: 14864 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
@@ -11,19 +11,15 @@ public class Main {
         int[] minArray = new int[n+1];
 
         for(int i=1; i<n+1; i++) {
-            if(Math.sqrt(i) % 1 == 0 ){
+            if(Math.sqrt(i) % 1 == 0 ){     // 제곱수 세팅
                 minArray[i] = 1;
             }else{
-                int min = n;
+                minArray[i] = n;
                 for(int j=1; j<=i/2; j++){
-                    int sum = minArray[j] + minArray[i-j];
-                    if(min > sum){
-                        min = sum;
-                    }
+                    minArray[i] = Math.min(minArray[i], minArray[j] + minArray[i-j]);
                 }
-                minArray[i] = min;
             }
-        }// 코드가 맘에 안드는데 고칠려니 너무 어려워.
+        }
 
             bw.write(minArray[n] + "");
 
