00a4ee6958e4f11fadab3410828d850aa8fdf723
김지윤 <117561820+kimjiyooniiiii@users.noreply.github.com>
2024-01-28 21:32:38 +0900

    [Silver II] Title: N과 M (9), Time: 1348 ms, Memory: 39708 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+
+    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static int n, m;
+    public static boolean[] isVisited;
+    public static int[] array;
+    public static int[] nArray;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        n = Integer.parseInt(input[0]);
+        m = Integer.parseInt(input[1]);
+        array = new int[m];
+        nArray = new int[n];
+        isVisited = new boolean[n];
+
+        String[] nInput = br.readLine().split(" ");
+        for(int i=0; i<n; i++) {
+            nArray[i] = (Integer.parseInt(nInput[i]));
+        }
+
+        Arrays.sort(nArray);
+        recur(0);
+
+        bw.flush();
+        bw.close();
+    }
+
+    public static void recur(int len) throws IOException {
+        if(len == m) {
+            for(int a : array) {
+                bw.write(a + " ");
+                bw.flush();
+            }
+            bw.write("\n");
+            bw.flush();
+        } else {
+            int prev = 0;
+            for(int i=0; i<n; i++) {
+                if(!isVisited[i] && prev != nArray[i]){
+                    isVisited[i] = true;
+                    prev = nArray[i];
+                    array[len] = nArray[i];
+                    recur(len + 1);
+
+                    isVisited[i] = false;
+                }
+
+            }
+        }
+    }
+
+
+}
