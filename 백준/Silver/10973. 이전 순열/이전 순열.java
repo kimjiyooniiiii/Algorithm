235115ef8c686999a9acb735fcfabed4f1adbb11
김지윤 <117561820+kimjiyooniiiii@users.noreply.github.com>
2024-01-30 14:05:50 +0900

    [Silver III] Title: 이전 순열, Time: 212 ms, Memory: 17492 KB -BaekjoonHub

--- 백준/Silver/10973. 이전 순열/이전 순열.java
+++ 백준/Silver/10973. 이전 순열/이전 순열.java
@@ -1,86 +1,49 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public class Main {
 
-    static List<String> numList = new ArrayList<>();
-    static int n;
-    static boolean[] isVisited;
-    static int[] makeArray;
-    static String search = "";
+    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        //1~n까지 이루어지는 순열
-        n = Integer.parseInt(br.readLine());
-        /*isVisited = new boolean[n+1];
-        makeArray = new int[n+1];
-
-        make(0);
-
-        System.out.println(numList);*/
-
+        int n = Integer.parseInt(br.readLine());
         String[] input = br.readLine().split(" ");
-        Integer[] intInput = new Integer[n];
-        for(int i=0; i<n; i++){
-            intInput[i] = Integer.parseInt(input[i]);
+        int[] first = new int[input.length];
+
+        for(int i=0; i<input.length; i++) {
+            first[i] = Integer.parseInt(input[i]);
         }
 
-        for(int i=intInput.length-1; i>=0; i--){
-            if(i==0){
-                    System.out.print(-1);
-                    return;
+        // 오름차순의 시작을 찾음
+        int i = first.length-1;
+        while(i > 0 && first[i-1] < first[i]) i--;
+
+        int j = first.length-1;
+        if(i == 0) bw.write("-1");
+        else {
+            while(first[i-1] < first[j]) j--;
+            swap(first, i-1, j);
+
+            j = first.length-1;
+            while(i < j){
+                swap(first, i, j);
+                i++; j--;
             }
-            int current = intInput[i];
-            int prev = intInput[i-1];
 
-            if(prev > current){
-                Arrays.sort(intInput, i,intInput.length, Collections.reverseOrder());
-                int max = 0;
-                int maxIndex = 0;
-                for(int j=i; j<intInput.length; j++){
-                    if(intInput[j] > max && intInput[j] < prev){
-                        max = intInput[j];
-                        maxIndex = j;
-                    }
-                }
-                intInput[i-1] = max;
-                intInput[maxIndex] = prev;
-
-                for(int x=0; x<intInput.length; x++){
-                    if(x == intInput.length-1){
-                        System.out.print(intInput[x] + "\n");
-                    }else{
-                        System.out.print(intInput[x]+" ");
-                    }
-                }
-                return;
+            for(int f : first){
+                bw.write(f + " ");
             }
         }
+
+        bw.flush();
+        bw.close();
     }
 
-    /*public static void make(int r){
-        if(r == n){
-            String result = "";
-            for(int i=1; i<makeArray.length; i++){
-                result += String.valueOf(makeArray[i]);
-            }
-            numList.add(result);
-            return;
-        }else{
-            for(int i=1; i<=n; i++){
-                if(isVisited[i] == false){
-                    isVisited[i] = true;
-                    makeArray[r+1] = i;
-                    make(r+1);
-                    isVisited[i] = false;
-                }
-            }
-        }
-    }*/
+    public static void swap(int[] array, int index1, int index2) {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+
 }
