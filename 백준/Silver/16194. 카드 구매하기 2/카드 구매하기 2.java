a028f5016b5bb3bd0ad5819e1eac6e4c0fa2336e
김지윤 <117561820+kimjiyooniiiii@users.noreply.github.com>
2024-02-09 15:27:00 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 152 ms, Memory: 14776 KB -BaekjoonHub

---
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+public class Main {
+
+    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static int n, max = 0;
+
+    public static int[][] matrix;
+
+    public static int[] price;
+    public static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        price = new int[n+1];
+        dp = new int[n+1];
+
+        String[] input = br.readLine().split(" ");
+        for(int i=1; i<=n; i++) {
+            price[i] = Integer.parseInt(input[i-1]);
+            dp[i] = price[i];
+        }
+
+        for(int i=0; i<=n; i++) {
+            for(int j=0; j<i; j++) {
+                dp[i] = Math.min(dp[i], dp[j] + price[i-j]);
+            }
+        }
+
+            bw.write(dp[n] + "");
+
+        bw.flush();
+        bw.close();
+    }
+}
