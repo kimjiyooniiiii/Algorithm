b968f403768372f677955d2d7706a531b6e3c6d6
김지윤 <117561820+kimjiyooniiiii@users.noreply.github.com>
2024-02-14 16:47:53 +0900

    [Silver I] Title: 1, 2, 3 더하기 2, Time: 148 ms, Memory: 15920 KB -BaekjoonHub

--- 백준/Silver/12101. 1， 2， 3 더하기 2/1， 2， 3 더하기 2.java
+++ 백준/Silver/12101. 1， 2， 3 더하기 2/1， 2， 3 더하기 2.java
@@ -1,11 +1,11 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Comparator;
 
 public class Main {
 
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     public static int n, k = 0;
+    public static int[] array;
+    public static Long count = 0L;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,34 +14,35 @@ public class Main {
         n = Integer.parseInt(input[0]);
         k = Integer.parseInt(input[1]);
 
-        ArrayList<String>[] array = new ArrayList[11];
-        for(int i=0; i<11; i++) {
-            array[i] = new ArrayList<>();
-        }
-        array[1].add("1");
-        array[2].add("1+1");
-        array[2].add("2");
-        array[3].add("1+1+1");
-        array[3].add("1+2");
-        array[3].add("2+1");
-        array[3].add("3");
+        array = new int[n];
+        recur(0,0);
 
-        for(int i=4; i<=n; i++) {
-            for(int j=1; j<=3; j++) {
-                for(String s : array[i-j]){
-                    array[i].add(s + "+" + j);
+        if(count < 2147483648L){
+            System.out.println("-1");
+        }
+        bw.flush();
+        bw.close();
+    }
+
+    public static void recur(int sum, int position) {
+        if(sum == n){
+            count++;
+            if(k == count){
+                for(int i=0; i<position-1; i++) {
+                    System.out.print(array[i] + "+");
+                }
+                System.out.print(array[position-1] + "\n");
+                count = 2147483648L;
+            }
+        } else {
+            for(int i=1; i<=3; i++) {
+                if(sum+i <= n){
+                    array[position] = i;
+                    recur(sum + i, position + 1);
+                } else {
+                    break;
                 }
             }
         }
-
-        if(array[n].size() < k) {
-            bw.write("-1");
-        } else {
-            array[n].sort(Comparator.naturalOrder());
-            bw.write(array[n].get(k-1));
-        }
-
-        bw.flush();
-        bw.close();
     }
 }
