8d0d98a1568318b07453d9d861809e68c3df929e
김지윤 <117561820+kimjiyooniiiii@users.noreply.github.com>
2024-09-04 14:09:14 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 10.29 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,49 +1,26 @@
+// 1 : 57
 class Solution {
-    public static int zero = 0;
-    int change = 0;
     
     public int[] solution(String s) {
         int[] answer = new int[2];
-
-        while(!s.equals("1")) {
-            change++;
-            s = removeZero(s);
-            s = toBinary(s);
-
-        }
         
-        answer[0] = change;
-        answer[1] = zero;
+        while(s.length() > 1){
+            answer[0]++;
+            StringBuilder sb = new StringBuilder();
+            
+            for(int i=0; i<s.length(); i++){
+                if(s.charAt(i) == '0'){
+                    answer[1]++;
+                }else{
+                    sb.append('1');
+                }
+            }
+            s = Integer.toBinaryString(sb.toString().length());
+        }
 
         return answer;
     }
-    
-    public String removeZero(String s) {
-        StringBuilder sb = new StringBuilder();
-        
-        for(int i=0; i<s.length(); i++) {
-            if(s.charAt(i) == '0'){
-                zero++;
-            }else{
-                sb.append(s.charAt(i));
-            }
-        }
-        return sb.toString();
-    }
-    
-    public String toBinary(String s) {
-        StringBuilder sb = new StringBuilder();
-        
-        int len = s.length();
-        int count = len;
-        
-        while(len > 1) {
-            count = len/2;
-            int spare = len%2;
-            sb.append(String.valueOf(spare));
-            len = len/2;
-        }
-        sb.append(1);
-        return sb.reverse().toString();
-    }
+
 }
+
+// 111111 
