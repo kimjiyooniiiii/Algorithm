bfe19d9bb6cb9e6e33c8b1e5ee1bbe5bd4783ab2
김지윤 <117561820+kimjiyooniiiii@users.noreply.github.com>
2024-09-05 19:58:30 +0900

    [level 2] Title: 구명보트, Time: 10.57 ms, Memory: 56.3 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -2,33 +2,25 @@ import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
-        
         Arrays.sort(people);
+        
         int start = 0, end = people.length-1;
         
-        while(true){
-           // System.out.println("start " + start + " end " + end);
-        if(start == end){
-            answer++;
-            break;
-        }else if(start > end){
-            break;
-        }else{
-           // System.out.println("sum " + (people[start] + people[end]));
+        while(start <= end){
+            if(start == end){
+                answer++;
+                break;
+            }
             if(people[start] + people[end] <= limit){
                 start++;
-                end--;
-                answer++;
-            }else{
-                end--;
-                answer++;
-            }   
+            }
+            end--; 
+            answer++;
         }
-            //System.out.println("answer " + answer);
-            
-        }
-        
         
         return answer;
     }
 }
+
+// 7 : 43
+
