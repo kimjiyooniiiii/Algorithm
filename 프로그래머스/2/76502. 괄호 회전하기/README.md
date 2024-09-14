504a002579ece7cca5b26caf677fa341623fbd61
김지윤 <117561820+kimjiyooniiiii@users.noreply.github.com>
2024-09-14 23:19:33 +0900

    [level 2] Title: 괄호 회전하기, Time: 78.44 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/README.md
+++ 프로그래머스/2/76502. 괄호 회전하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 괄호 회전하기 - 76502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502) 
 
 ### 성능 요약
 
-메모리: 84.3 MB, 시간: 0.42 ms
+메모리: 88.4 MB, 시간: 78.44 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 15일 20:47:15
+2024년 09월 14일 23:19:30
 
 ### 문제 설명
 
--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,71 +1,46 @@
-// 시간복잡도 O(n^2)
-// 공간복잡도 O(n)
 
 import java.util.*;
 
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        // 컬렉션을 선언하기 위해 char의 wrapper 클래스인 Character를 사용한다.
-        Deque<Character> letters = new LinkedList<>();
+        int len = s.length();
         
-        for(int i=0; i<s.length(); i++){
-            letters.add(s.charAt(i));
-        }
-        
-        // 왼쪽으로 회전 시켜서 올바른 괄호인지 확인하기
-        for(int i=0; i<s.length(); i++){
-            if(i != 0){
-                letters.addLast(letters.pollFirst());
-            }
-            // 참조 변수 깊은 복사하기
-            Deque<Character> copy = new LinkedList<>();
-            copy.addAll(letters);
-            
-            // 올바른 괄호인지 확인하기
-            if(isCorrect(copy)){
-                answer++;
-            }
+        for(int i=0; i<len; i++){   // i만큼 왼쪽 회전
+            answer += count(i, s);
         }
             
         return answer;
     }
     
-    public boolean isPair(char current, char peek){
-        char[] open = {'{','(','['};
-        char[] close = {'}',')',']'};
+    public int count(int left, String s){
+        Stack<Character> stack = new Stack<>();
         
-        for(int i=0; i<3; i++){
-            if(open[i] == current && close[i] == peek){
-                return true;
-            }
+        for(int i=left; i<s.length(); i++){ 
+            char c = s.charAt(i);
+            check(stack, c);
         }
-        return false;
+        for(int i=0; i<left; i++){
+            char c = s.charAt(i);
+            check(stack, c);
+        }
+        
+        if(stack.isEmpty()) return 1;
+        else return 0;
+    }
+    
+    public void check(Stack<Character> stack, char c){
+        if(stack.isEmpty()){
+                stack.push(c);
+            }
+            else if(c == '[' || c == '(' || c == '{'){
+                stack.push(c);
+            }
+            else if(c == ']' && stack.peek() == '['
+              || c == '}' && stack.peek() == '{'
+              || c == ')' && stack.peek() == '('){
+                stack.pop();
+            }
     }
 
-    // 함수를 분리하여 코드의 재사용성을 높이고, 가독성을 높이겠습니다.
-    public boolean isCorrect(Deque<Character> letters){
-        // 짝이 맞는 괄호가 있는지 확인하기 위해 괄호를 저장해놓는 임시 스택
-        Stack<Character> compare = new Stack<>();
-        
-        while(!letters.isEmpty()){
-            char current = letters.pollLast();
-            if(compare.isEmpty()){
-                compare.push(current);
-            }
-            else{
-                // 비교군의 최근 괄호와 짝이 맞고, 열려있는 괄호라면 비교군에서 삭제
-                if(isPair(current,compare.peek())){ 
-                    compare.pop();
-                }else{
-                    compare.push(current);
-                }
-            }
-        }
-        // 모든 비교가 끝난 후, 짝이 안맞는 괄호가 남아있다면 올바른 괄호 배열이 아님
-        if(compare.size() != 0){    
-            return false;
-        }
-        return true;
-    }
 }
