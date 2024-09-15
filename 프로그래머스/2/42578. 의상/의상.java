e648705869eed4427836a2447f4e452536037941
김지윤 <117561820+kimjiyooniiiii@users.noreply.github.com>
2024-09-15 16:50:19 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -3,42 +3,18 @@ import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
+        Map<String, Integer> type = new HashMap<>();
         
-        HashMap<String, List<String>> map = new HashMap<>();
-        for(String[] c : clothes){
-            if(map.containsKey(c[1])){
-                List<String> value = map.get(c[1]);
-                value.add(c[0]);
-                map.replace(c[1],value);
-            }else{
-                List<String> value = new ArrayList<>();
-                value.add(c[0]);
-                map.put(c[1],value);
-            }
-        }
-        // Iterator<Map.Entry<String, List<String>>> it = map.entrySet().iterator();
-        // while(it.hasNext()){
-        //     Map.Entry<String, List<String>> next = it.next();
-        //     System.out.println(next.getKey() + " " + next.getValue());
-        // }
-        
-        for(Map.Entry<String, List<String>> m : map.entrySet()){
-            answer *= (m.getValue().size() + 1);
+        for(String[] cloth : clothes){
+            String key = cloth[1];
+            type.put(key, type.getOrDefault(key, 0) + 1);
         }
         
+        for(String k : type.keySet()){
+            answer *= (type.get(k) + 1);
+        }
+       
         return answer-1;
     }
     
-    public int factorial(int num, int result){
-        if(num == 1){
-            return result;
-        }else{
-            result *= num;
-            return factorial(num-1, result);
-        }
-    }
-    
-    public int sameType(int num) {
-        return num * (num-1) / 2;
-    }
 }
