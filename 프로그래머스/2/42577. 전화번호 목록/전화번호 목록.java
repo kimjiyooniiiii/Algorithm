e796486e1a02111e3a6c17fc2a56452846bcb878
김지윤 <117561820+kimjiyooniiiii@users.noreply.github.com>
2024-09-18 22:00:06 +0900

    [level 2] Title: 전화번호 목록, Time: 312.13 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,22 +1,24 @@
+// 9:45 -> 59
 import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
+        Arrays.sort(phone_book);
         
-        HashSet<String> set = new HashSet<>();
-        for(String p : phone_book){
-            set.add(p);
-        }
-        
-        for(String p : phone_book){
-            for(int i=1; i<p.length(); i++){
-                if(set.contains(p.substring(0,i))){
-                    answer = false;
-                    return answer;
-                }
+        for(int i=0; i<phone_book.length-1; i++){
+            if(phone_book[i].length() > phone_book[i+1].length()){
+                continue;
+            }
+            int size = phone_book[i].length();
+            String front = phone_book[i+1].substring(0,size);
+           // System.out.println(phone_book[i] + " " + front);
+            if(front.equals(phone_book[i])){
+                answer = false;
             }
         }
         return answer;
     }
 }
+// 완전 치명적인 실수 했네.. 그냥 포함이 아니고 앞에 포함.. 하..
+// 이걸 왜 놓쳤을까. 빨리 풀고 싶어서. 
