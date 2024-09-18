f9a9aa89666fa806edb7e29515ffa95ea0bdc2ff
김지윤 <117561820+kimjiyooniiiii@users.noreply.github.com>
2024-09-18 21:43:50 +0900

    [level 2] Title: 프로세스, Time: 1.20 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,44 +1,36 @@
 import java.util.*;
 class Solution {
-    class Process{
-        private int name;
-        private int priority;
-        public Process(int name, int priority){
-            this.name = name;
-            this.priority = priority;
-        }
-    }
-    
+
     public int solution(int[] priorities, int location) {
-        int answer = 0;
-        Queue<Process> processQ = new LinkedList<>();    // 2 1 3 2
+        int answer = 1;
+        Queue<int[]> process = new LinkedList<>();      // [순위, 인덱스]
         for(int i=0; i<priorities.length; i++){
-            processQ.add(new Process(i,priorities[i]));
+            process.add(new int[]{priorities[i],i});
         }
+        Arrays.sort(priorities);
         
-        Arrays.sort(priorities);    //  3 2 2 1 실행되어야 하는 우선순위
+        int curPriorityIndex = priorities.length-1;     // 현재 가장 높은 우선 순위의 위치
         
-        answer = run(priorities, location, processQ);
+        while(!process.isEmpty()){
+            int[] p = process.poll();
+            int curPriority = priorities[curPriorityIndex]; 
+            //System.out.println(curPriority + " " + location);
+                
+            if(p[0] == curPriority && p[1] == location){
+                //System.out.println("찾음 " + curPriority + " " + location);
+                return answer;
+            }
+            if(p[0] != curPriority){
+                //System.out.println("후순위 " + p[0] + " " + p[1]);
+                process.add(p);
+            }else{
+                //System.out.println("먼저 나감 " + curPriority + " " + p[1]);
+                curPriorityIndex--;
+                answer++;
+            }
+        }
+
         return answer;
     }
-    
-    public int run(int[] priorities, int location, Queue<Process> processQ){
-        int current = priorities.length-1;    // 현재 실행되어야 하는 우선순위
-        int result = 1;
-        while(!processQ.isEmpty()){
-            Process p = processQ.poll();
-            // 큐에서 나온 프로세스의 우선순위가 현재 실행해야 하는 우선순위와 같은지 비교
-            if(priorities[current] == p.priority){  
-                if(p.name == location){ // 찾고자하는 프로세스 였다면 몇번째 실행인지 반환
-                    return result;
-                }
-                result++;   // 프로세스 실행 후
-                current--;
-            }
-            else{   // 아직 순서가 되지 않은 프로세스라면
-                processQ.add(p);
-            }
-        }
-        return -1;
-    }
+
 }
