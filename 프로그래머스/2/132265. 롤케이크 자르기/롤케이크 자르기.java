ad3f5dbaf555c6f28ff0f615017840d225e00d13
김지윤 <117561820+kimjiyooniiiii@users.noreply.github.com>
2024-09-21 14:22:45 +0900

    [level 2] Title: 롤케이크 자르기, Time: 250.15 ms, Memory: 166 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -1,32 +1,28 @@
+// 2:00 -> 12 -> 19
 import java.util.*;
 class Solution {
     public int solution(int[] topping) {
         int answer = 0;
-        Map<Integer,Integer> one = new HashMap<>();
-        Map<Integer,Integer> two = new HashMap<>();
         
-        for(int i=0; i<topping.length; i++){
-            int type = topping[i];
-            one.put(type, one.getOrDefault(type,0)+1);
+        Map<Integer, Integer> brother = new HashMap<>();
+        for(int t : topping){
+            brother.put(t, brother.getOrDefault(t, 0) + 1);
         }
         
+        Set<Integer> chulsu = new HashSet<>();
+        
         for(int i=0; i<topping.length; i++){
-            int type = topping[i];
-            two.put(type, two.getOrDefault(type,0)+1);
-            
-            int value = one.get(type) - 1;
-            if(value == 0){
-                one.remove(type);
-            }else{
-                one.put(type, value);
+            int key = topping[i];
+            chulsu.add(key);
+            if(brother.containsKey(key)){
+                int value = brother.get(key);
+                if(value == 1) brother.remove(key);
+                else brother.put(key, value - 1);
             }
             
-            if(one.size() == two.size()){
-                answer++;
-            }
+            if(chulsu.size() == brother.size()) answer++;
         }
+        
         return answer;
     }
 }
-
-// 한 개가 남았을 때 
