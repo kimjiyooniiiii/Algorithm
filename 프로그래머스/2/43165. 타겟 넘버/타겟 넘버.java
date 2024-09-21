e3c7c54a9df0cd379fcc627fbeeea4b58615fbb7
김지윤 <117561820+kimjiyooniiiii@users.noreply.github.com>
2024-09-21 15:19:51 +0900

    [level 2] Title: 타겟 넘버, Time: 6.58 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,25 +1,22 @@
+// 2:54 -> 3:20
 import java.util.*;
 
 class Solution {
-    private boolean[][] visit;
-    private int[][] matrix;
-    private int answer = 0;
-    
+    private int answer = 0;    
     public int solution(int[] numbers, int target) {
-        dfs(numbers, 0, 0, target);
-      
+        dfs(numbers, target, 0, 0);
         return answer;
     }
     
-    public void dfs(int[] numbers, int sum, int depth, int target){
-        if(depth == numbers.length){
+    public void dfs(int[] numbers, int target, int depth, int sum){
+        if(depth >= numbers.length){
             if(sum == target){
                 answer++;
             }
             return;
-        }
-        dfs(numbers, sum+numbers[depth], depth+1, target);
-        dfs(numbers, sum-numbers[depth], depth+1, target);
+        } 
+        dfs(numbers, target, depth+1, sum + numbers[depth]);
+        dfs(numbers, target, depth+1, sum + (-1) * numbers[depth]);
     }
 }
 
