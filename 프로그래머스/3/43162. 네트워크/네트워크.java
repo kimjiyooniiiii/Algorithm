3322e71c5541c17bfe0f8dd736ffcdff329736a1
김지윤 <117561820+kimjiyooniiiii@users.noreply.github.com>
2024-09-23 10:53:16 +0900

    [level 3] Title: 네트워크, Time: 0.26 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,32 +1,29 @@
+// 10:22
 import java.util.*;
 class Solution {
+    private boolean[] visit;
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        boolean[][] visit = new boolean[n][n];
+        visit = new boolean[n];
         
         for(int i=0; i<n; i++){
-                if(!visit[i][i]){
-                    visit[i][i] = true;
-                    answer++;
-                    //System.out.println(i + " -> " + i);
-                    Queue<Integer> q = new LinkedList<>();
-                    q.add(i);
-                    bfs(q, n, visit, computers);
-                    //System.out.println("-------------");
-                }
+            if(!visit[i]){
+                visit[i] = true;
+                answer++;
+                dfs(n, computers, answer, i);
+            }
         }
+        
         return answer;
     }
-    public void bfs(Queue<Integer> q, int n, boolean[][] visit, int[][] computers){
-        while(!q.isEmpty()){
-            int poll = q.poll();
-            for(int j=0; j<n; j++){
-                if(!visit[poll][j] && computers[poll][j] == 1){
-                    visit[poll][j] = true;
-                    q.add(j);
-                    //System.out.println(poll + " -> " + j);
-                }
+    
+    public void dfs(int n, int[][] computers, int answer, int i){
+        for(int j=0; j<n; j++){
+            if(!visit[j] && computers[i][j] == 1){
+                visit[j] = true;
+                dfs(n, computers, answer, j);
             }
         }
     }
 }
+// 방문 자체가 없다면 다음에 방문할 수 있다.
