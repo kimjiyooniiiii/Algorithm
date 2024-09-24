abb3f9d65a44677563b813fb31c1fa47171b283b
김지윤 <117561820+kimjiyooniiiii@users.noreply.github.com>
2024-09-24 15:56:03 +0900

    [level 2] Title: 주식가격, Time: 58.96 ms, Memory: 66 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,26 +1,30 @@
+// 뒤 작은 수
 import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        Stack<Integer> compare = new Stack<>();
-        compare.push(0);
+        Stack<Integer> stack = new Stack<>();
         
-        for(int i=1; i<prices.length; i++){
-            while(!compare.isEmpty() && prices[compare.peek()] > prices[i]){
-                int prev = compare.pop();   // 이전 가격 인덱스
-                answer[prev] = i - prev;
+        for(int i=0; i<prices.length; i++){
+            int count = 1;
+            while(!stack.isEmpty()){
+                int prevI = stack.peek();
+                if(prices[prevI] > prices[i]){
+                    //answer[stack.pop()] = count++;
+                    answer[stack.pop()] = i - prevI;
+                }else break;
             }
-            compare.push(i);
+            stack.push(i);
         }
         
-        while(!compare.isEmpty()){
-            int i = compare.pop();
-            answer[i] = prices.length - 1 - i;
+        for(int i=0; i<answer.length; i++){
+            if(answer[i] == 0){
+                answer[i] = answer.length - i - 1;
+            }
         }
-        
         return answer;
     }
 }
 
-// 뒤에 자기보다 작은 수가 있다면 (그 인덱스 - 자신의 인덱스)를 결과에 넣기
-// 그렇지 않다면 (배열 길이 - 1 - 자신의 인데스) 넣기
+// 같은 배열 안에서 여러개와 비교할 때는 스택을 사용하는 거 같다.
+// 1,2,2,0,3    [0,0,1,0,0] 전체 길이 - 자신의 인덱스 - 1
