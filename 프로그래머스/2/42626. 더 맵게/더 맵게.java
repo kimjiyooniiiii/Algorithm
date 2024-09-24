cfc2e5219905c0c5a368bee9d709022d3240e27b
김지윤 <117561820+kimjiyooniiiii@users.noreply.github.com>
2024-09-24 15:03:33 +0900

    [level 2] Title: 더 맵게, Time: 1313.71 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -3,36 +3,26 @@ import java.util.*;
 class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
-        
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
-        for(int i=0; i<scoville.length; i++) {
-            queue.offer(scoville[i]);
+        Queue<Integer> q = new PriorityQueue<>();
+        boolean isZero = true;
+        for(int s : scoville){
+            q.add(s);
         }
         
-        while(!queue.isEmpty()){
-            // 모든 음식의 스코빌 지수를 K 이상으로 만들 수 없을 때
-            if(queue.size() == 1 && queue.peek() < K){
+        while(!q.isEmpty()){
+            if(q.peek() >= K) break;
+            
+            if(q.size() == 1) {
                 answer = -1;
                 break;
             }
             
-            // 제일 안 매운 음식
-            int first = queue.poll();
-            
-            // 제일 안 매운 음식의 스코빌 지수가 K 이상일 때
-            if(first >= K){
-                break;
-            }
-            
-            // 두번 째로 안 매운 음식과 섞는다.
-            if(!queue.isEmpty()){
-                int second = queue.poll();
-                queue.offer(first + second * 2);
-                answer++;
-            }
-            
+            q.add(q.poll() + q.poll() * 2);
+            answer++;
         }
         
         return answer;
     }
 }
+
+// 배열에 있는 수를 반복해서 가장 작은 수를 꺼내야함 -> 우선순위 큐
