45f7df9562ae8c8b4c7cc6424f3bc792c91864a0
김지윤 <117561820+kimjiyooniiiii@users.noreply.github.com>
2024-09-27 17:18:22 +0900

    [level 3] Title: 야근 지수, Time: 139.56 ms, Memory: 67.9 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -1,27 +1,25 @@
+// 5:00
 import java.util.*;
 class Solution {
     public long solution(int n, int[] works) {
         long answer = 0;
-        PriorityQueue<Integer> oper = new PriorityQueue<>(Comparator.reverseOrder());
-        for(int w : works){
-            oper.add(w);
-        }
+        
+        PriorityQueue<Integer> q = new PriorityQueue<>(Comparator.reverseOrder());
+        for(int w : works) q.add(w);
         
         for(int i=0; i<n; i++){
-            if(!oper.isEmpty()){
-                int work = oper.poll();
-                if(--work > 0){
-                    oper.add(work);
-                }
-            }
+            if(q.isEmpty()) break;
+            int peek = q.peek();
+            //System.out.println(peek);
+            
+            if(peek > 1) q.add(q.poll() - 1);
+            else q.poll();
         }
         
-        while(!oper.isEmpty()){
-            int work = oper.poll();
-            answer += (work * work);
+        while(!q.isEmpty()){
+            answer += Math.pow(q.poll(),2);
         }
         
         return answer;
     }
 }
-// 야근을 시작한 시점에 양이 작아지도록
