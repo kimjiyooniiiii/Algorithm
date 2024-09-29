b53675f56713dedc3265e89427a2aaf7da472b31
김지윤 <117561820+kimjiyooniiiii@users.noreply.github.com>
2024-09-29 20:01:46 +0900

    [level 3] Title: 단어 변환, Time: 0.43 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,52 +1,31 @@
 import java.util.*;
 class Solution {
     public int solution(String begin, String target, String[] words) {
-        int answer = 0;
-        Queue<int[]> q = new LinkedList<>();
-        boolean[] isVisit = new boolean[words.length];
-        
-        for(int i=0; i<words.length; i++){
-            int diff = 0;
-            for(int j=0; j<begin.length(); j++){
-                if(begin.charAt(j) != words[i].charAt(j)){
-                    diff++;
-                }
-            }
-            if(diff == 1){
-                if(words[i].equals(target)){
-                    return 1;
-                }
-                isVisit[i] = true;
-                q.add(new int[]{i,1}); // 인덱스와 카운트
-                //System.out.println("q.add " + i + " " + (1));
-            }
-        }
-        answer = bfs(q, isVisit, begin, target, words);
-        
-        return answer;
+
+        return bfs(begin, target, words);
     }
     
-    public int bfs(Queue<int[]> q, boolean[] isVisit, String begin, String target, String[] words){
+    public int bfs(String begin, String target, String[] words){
+        boolean[] visit = new boolean[words.length];
+        Queue<String[]> q = new LinkedList<>();
+        q.add(new String[]{begin,"0"});
+        
         while(!q.isEmpty()){
-            int[] poll = q.poll();
-            String prev = words[poll[0]];
-            int count = poll[1];
+            String[] prev = q.poll();
+            if(prev[0].equals(target)) return Integer.parseInt(prev[1]);
             
             for(int i=0; i<words.length; i++){
-                if(!isVisit[i]){
+                if(!visit[i]){
+                    String cur = words[i];
                     int diff = 0;
-                    for(int j=0; j<prev.length(); j++){
-                        if(prev.charAt(j) != words[i].charAt(j)){
-                            diff++;
-                        }
+                    for(int j=0; j<cur.length(); j++){
+                        if(prev[0].charAt(j) != cur.charAt(j)) diff++;
                     }
                     if(diff == 1){
-                        if(words[i].equals(target)){
-                            return count+1;
-                        }
-                        isVisit[i] = true;
-                        q.add(new int[]{i,count+1}); // 인덱스와 카운트
-                        //System.out.println("q.add " + i + " " + (count+1));
+                       // System.out.println(cur);
+                        visit[i] = true;
+                        q.add(new String[]
+                              {cur, String.valueOf(Integer.parseInt(prev[1])+1)});
                     }
                 }
             }
@@ -54,5 +33,3 @@ class Solution {
         return 0;
     }
 }
-
-// 일차원 배열을 순회하면서 하나 차이나는 것을 구할 수 있다니. 신기하다...
