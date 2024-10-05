aec2d349d4d4866c7bbaa92efe4a8c514b5a5302
김지윤 <117561820+kimjiyooniiiii@users.noreply.github.com>
2024-10-05 13:45:20 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 26.00 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,24 +1,44 @@
+// 6:32
 import java.util.*;
 class Solution {
-    private int answer = 0;
-    Queue<Integer> bridge = new LinkedList<>();
+    private Queue<Integer> q = new LinkedList<>();
+    private Queue<Integer> location = new LinkedList<>();
+    private int time = 0, index = 0, curWeight = 0;
+    
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        
-        for(int i=0; i<truck_weights.length; i++){
-            if(bridge.size() == bridge_length){
-                weight += bridge.poll();
-            }
-            if(weight < truck_weights[i]){
-                bridge.add(0);
-                answer++;
-                i--;
-            }else{
-                bridge.add(truck_weights[i]);
-                weight -= truck_weights[i];
-                answer++;
-            }
-        }
-        return answer + bridge_length;
-    }
+        for(int i=0; i<bridge_length; i++)  location.add(0);
 
+        int count = 0;
+        while(true){
+            if(index >= truck_weights.length) {
+                time += bridge_length;
+                break;
+            }
+            int w = truck_weights[index];
+            int isTruck = location.poll();
+            
+            if(isTruck == 1){
+                //System.out.println("무게 빼기 " + q.peek());
+                curWeight -= q.poll();
+            }
+            
+            if(q.size() < bridge_length && curWeight + w <= weight){    
+                plus(w);
+            }else{
+                location.add(0);
+            }
+            time++;
+            count++;
+            //System.out.println("시간 " + time + "------------");
+        }
+        //System.out.println("끝 시간 " + time);
+        return time;
+    }
+    public void plus(int w){
+        curWeight += w;
+        q.add(w);
+        index++;
+        location.add(1);
+        //System.out.println("무게 추가 " + w + " 총 " + curWeight);
+    }
 }
