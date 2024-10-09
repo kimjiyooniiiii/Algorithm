2f01fc93c39d59005d6d9f03e302b3d8238238a2
김지윤 <117561820+kimjiyooniiiii@users.noreply.github.com>
2024-10-09 21:10:46 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 71.30 ms, Memory: 154 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -1,50 +1,31 @@
+// 8:10
 import java.util.*;
 class Solution {
     public int[] solution(int[] sequence, int k) {
-        int[] answer = new int[2];
-        List<Result> passResult = new ArrayList<>();
-        Queue<Integer> element = new LinkedList<>();
+        int start=0, end=0, sum = sequence[0];
+        List<int[]> list = new ArrayList<>();
         
-        int sum = 0;
-        for(int i=0; i<sequence.length; i++){
-            sum += sequence[i];
-            element.add(i);
-            //System.out.println(sequence[i]);
-            
-            while(sum > k){
-                //System.out.println("빼기 " + sequence[element.peek()]);
-                sum -= sequence[element.poll()];
-            }
-            if(sum == k){
-               // System.out.println("완성 사이즈" + element.size() + " 시작" + sequence[element.peek()]);
-               // System.out.println(element.poll());
-                passResult.add(new Result(element.size(), element.peek()));
+        while(true){
+            if(start >= sequence.length || end >= sequence.length)  break;
+
+            if(sum < k){ 
+                if(++end >= sequence.length)  break;
+                sum += sequence[end];
+            }else if(sum > k){
+                sum -= sequence[start++];
+            }else{
+                list.add(new int[]{start, end, end-start+1});
+                sum -= sequence[start++];
             }
         }
         
-        Collections.sort(passResult, new Comparator<Result>(){
-            @Override
-            public int compare(Result a, Result b){
-                if(a.len == b.len){
-                    return a.start - b.start;
-                }
-                return a.len - b.len;
-            }
+        Collections.sort(list, (a, b) -> {
+            if(a[2] == b[2])    return a[0] - b[0];
+            return a[2] - b[2];
         });
-        
-        answer[0] = passResult.get(0).start;
-        answer[1] = passResult.get(0).start + passResult.get(0).len - 1;
-        return answer;
+
+        return new int[]{list.get(0)[0], list.get(0)[1]};
     }
-    
-    class Result{
-        private int len;
-        private int start;
-        
-        public Result(int len, int start){
-            this.len = len;
-            this.start = start;
-        }
-    }
+
 }
 
